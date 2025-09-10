e7440e67a328d60190a390b1905d76ef933c0828
Prateekpppp <prateekpppp4@gmail.com>
2025-09-11 00:08:22 +0530

    p_description

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -2,40 +2,66 @@
 
 @section('body')
 
-    <div class="row items-start justify-start w-full">
-        <div class="prod_view flex flex-row col-lg-6 gap-3">
-            <div class="w-1/6 flex flex-col items-center gap-2">
-                <div class="">
-                    <img src="https://unblast.com/wp-content/uploads/2018/11/Vertical-Product-Box-Mockup-1-1600x1200.jpg" alt="" class="rounded-xl w-12 h-18">
-                </div>
-                <div class="">
-                    <img src="https://unblast.com/wp-content/uploads/2018/11/Vertical-Product-Box-Mockup-1-1600x1200.jpg" alt="" class="rounded-xl w-12 h-18">
-                </div>
-                <div class="">
-                    <img src="https://unblast.com/wp-content/uploads/2018/11/Vertical-Product-Box-Mockup-1-1600x1200.jpg" alt="" class="rounded-xl w-12 h-18">
-                </div>
-                <div class="">
-                    <img src="https://unblast.com/wp-content/uploads/2018/11/Vertical-Product-Box-Mockup-1-1600x1200.jpg" alt="" class="rounded-xl w-12 h-18">
-                </div>
+<div class="app_product py-3">
+    
+  <!--================Single Product Area =================-->
+  <div class="product_image_area section_padding">
+    <div class="container">
+      <div class="row s_product_inner justify-content-between">
+        <div class="col-lg-7 col-xl-7">
+          <div class="product_slider_img">
+            <div id="vertical">
+              <div data-thumb="img/product/single-product/product_1.png">
+                <img src="img/product/single-product/product_1.png" />
+              </div>
+              <div data-thumb="img/product/single-product/product_1.png">
+                <img src="img/product/single-product/product_1.png" />
+              </div>
+              <div data-thumb="img/product/single-product/product_1.png">
+                <img src="img/product/single-product/product_1.png" />
+              </div>
+              <div data-thumb="img/product/single-product/product_1.png">
+                <img src="img/product/single-product/product_1.png" />
+              </div>
             </div>
-            <div class="w-5/6">
-                <img class="w-full rounded-xl" src="https://unblast.com/wp-content/uploads/2018/11/Vertical-Product-Box-Mockup-1-1600x1200.jpg" alt="" srcset="">
-            </div>
+          </div>
         </div>
-        <div class="prod_description col-lg-6 flex flex-col gap-2">
-            <h3 class="product_name">Bewakoof@</h3>
-            <div class="prod_description text-m">Product description</div>
-            <div class="flex flex-row justify-between items-center">
-                <div class="prod_amount_details w-full flex flex-row items-center justify-start gap-3">
-                    <div class="prod_amount">$ 4</div>
-                    <div class="prod_amount_prev">$ 10</div>
-                    <div class="prod_amount_off">58%</div>
-                </div>
-                <div class="prod_rating flex flex-row items-center justify-between rounded-xl border-solid border-1 border-gray-300 p-1 px-2">
-                    <span class="mr-1"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 12 12" style="height: 16px; width: 16px;" class=" " stroke="none"><path fill="#FFD232" d="M5.58 1.15a.5.5 0 0 1 .84 0l1.528 2.363a.5.5 0 0 0 .291.212l2.72.722a.5.5 0 0 1 .26.799L9.442 7.429a.5.5 0 0 0-.111.343l.153 2.81a.5.5 0 0 1-.68.493L6.18 10.063a.5.5 0 0 0-.36 0l-2.625 1.014a.5.5 0 0 1-.68-.494l.153-2.81a.5.5 0 0 0-.11-.343L.781 5.246a.5.5 0 0 1 .26-.799l2.719-.722a.5.5 0 0 0 .291-.212L5.58 1.149Z"></path></svg></span> 4.5 
-                </div>
+        <div class="col-lg-5 col-xl-4">
+          <div class="s_product_text">
+            
+            <h3>Faded SkyBlu Denim Jeans</h3>
+            <h2>$149.99</h2>
+            <ul class="list">
+              <li>
+                <a class="active" href="#">
+                  <span>Category</span> : Household</a>
+              </li>
+              <li>
+                <a href="#"> <span>Availibility</span> : In Stock</a>
+              </li>
+            </ul>
+            <p>
+              First replenish living. Creepeth image image. Creeping can't, won't called.
+              Two fruitful let days signs sea together all land fly subdue
+            </p>
+            <div class="card_area d-flex justify-content-between align-items-center">
+              <div class="product_count">
+                <span class="inumber-decrement"> <i class="ti-minus"></i></span>
+                <input class="input-number" type="text" value="1" min="0" max="10">
+                <span class="number-increment"> <i class="ti-plus"></i></span>
+              </div>
+              <a href="#" class="btn_3">add to cart</a>
+              <a href="#" class="like_us"> <i class="ti-heart"></i> </a>
             </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
+  <!--================End Single Product Area =================-->
+
+  @include('includes.p_description')
+
+</div>
 
 @endsection
