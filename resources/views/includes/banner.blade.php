330f1becce4be81a9c5f2c8ab64a5c94f20b8f6d
Prateekpppp <prateekpppp4@gmail.com>
2025-09-15 21:55:29 +0530

    banner update

--- resources/views/includes/banner.blade.php
+++ resources/views/includes/banner.blade.php
@@ -1,7 +1,7 @@
 
 <!-- banner part start-->
 <div class="flex flex-row items-start overflow-x-auto overflow-hidden w-full app_scroller">
-    <img src="{{asset('img')}}/banner/banner.jpeg" alt="" srcset="" class="min-w-full">
+    <!-- <img src="{{asset('img')}}/banner/banner.jpeg" alt="" srcset="" class="min-w-full"> -->
     @foreach([1,2,3,4] as $i)
     <!-- <div class="min-w-full bg-cover bg-center h-[70vh] d-flex" style="background-image: url('{{asset('img')}}/banner/banner-{{$i}}.jpg');"> -->
 
