77c41e95d0ee4f4a83448c7316ba245ba1182a54
Prateekpppp <prateekpppp4@gmail.com>
2025-09-27 03:17:54 +0530

    product page updates

--- resources/views/includes/product_img.blade.php
+++ resources/views/includes/product_img.blade.php
@@ -1,22 +1,22 @@
-<div class="app_product_img w-full h-[75vh] overflow-y-scroll">
+<div class="app_product_img w-full overflow-y-scroll">
     <div class="app_row">
         <div class="col-md-6 p-0 m-0">
-            <img src="https://picsum.photos/id/424/450/900" alt="" srcset="">
+            <img src="https://picsum.photos/id/424/450/750" alt="" srcset="">
         </div>
         <div class="col-md-6 p-0 m-0">
-            <img src="https://picsum.photos/id/425/450/900" alt="" srcset="">
+            <img src="https://picsum.photos/id/425/450/750" alt="" srcset="">
         </div>
         <div class="col-md-6 p-0 m-0">
-            <img src="https://picsum.photos/id/426/450/900" alt="" srcset="">
+            <img src="https://picsum.photos/id/426/450/750" alt="" srcset="">
         </div>
         <div class="col-md-6 p-0 m-0">
-            <img src="https://picsum.photos/id/427/450/900" alt="" srcset="">
+            <img src="https://picsum.photos/id/427/450/750" alt="" srcset="">
         </div>
         <div class="col-md-6 p-0 m-0">
-            <img src="https://picsum.photos/id/428/450/900" alt="" srcset="">
+            <img src="https://picsum.photos/id/428/450/750" alt="" srcset="">
         </div>
         <div class="col-md-6 p-0 m-0">
-            <img src="https://picsum.photos/id/429/450/900" alt="" srcset="">
+            <img src="https://picsum.photos/id/429/450/750" alt="" srcset="">
         </div>
     </div>
 </div>
