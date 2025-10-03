f4644bc51c1aa853d169edee9e9a16aa04c56bc1
Prateekpppp <prateekpppp4@gmail.com>
2025-10-04 02:15:36 +0530

    navbar update

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -25,7 +25,7 @@
 
                             <div class="flex flex-row items-center justify-end gap-1">
                                 <div class="main-menu-item basis-autoe" id="ds_navLinks">
-                                    @if(session('username'))
+                                    @if(!session('username'))
                                     <a href="javascript:void(0)" class="btn customBg p-2 my-1 rounded-0 app_login">Login/Sign Up</a>
                                     @endif
                                 </div>
